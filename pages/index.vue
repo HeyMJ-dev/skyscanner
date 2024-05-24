0508e46363eebb4c04fd5b871db17c1aa726e967
Mhshariatipour_ir <mohammadhosein,sh1378@gmail.com>
2024-05-24 16:56:10 +0330

    add: slider

--- pages/index.vue
+++ pages/index.vue
@@ -25,8 +25,9 @@ loader.load().then(async () => {
   <div class="w-full h-full relative">
     <div id="map" class="w-full h-full bg-blue-200"></div>
 
-    <div class="absolute bg-red-400 left-0 top-0">
+    <div class="absolute bg-red-400 left-0 bottom-0 md:h-full md:w-[350px] w-full md:px-6 py-4">
       <!--  Import the component here  -->
+      <Slider/>
     </div>
   </div>
 
